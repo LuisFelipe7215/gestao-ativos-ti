d8b7c1e171b788c210923809f3454d2b351a2315
LuisFelipe7215 <170734954+LuisFelipe7215@users.noreply.github.com>
2026-07-08 11:02:34 -0300

    chore: initial project setup

---
+++ src/main/java/com/felipe/gestaoativosti/GestaoAtivosTiApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.gestaoativosti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoAtivosTiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoAtivosTiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/felipe/gestaoativosti/GestaoAtivosTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.gestaoativosti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoAtivosTiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
